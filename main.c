f82e70cb74a90e77be405bdbf4f55894718a9b30
Beagle User <debian@BeagleBone.localdomain>
2021-12-22 08:14:05 +0000

    added: macro FUNC_SELECT

--- main.c
+++ main.c
@@ -1,108 +1,13 @@
 #include "common.h"
 
 
-#if 0
-uint32_t select_chs(void)
-{
-    uint32_t chs = 0;
-    char str[64];
-    char *ptr = NULL;
-    printf("Please select pwm chs[0-%d]:(0,1,2,3...)\n", PWM_MAX_CHS-1);
-    fgets(str, sizeof(str), stdin);
-    if ((str[0] == 'q') || (str[0] == 'Q'))
-        return 0;
-
-    ptr = str;
-    while (*ptr) {
-        int i = atoi(ptr);
-        if (i < 32) {
-            chs |= ((uint32_t)0x01<<i);
-        }
-
-        ptr = strchr(ptr, ',');
-        if (NULL == ptr)
-            break;
-        ptr++;
-
-        if (('\r' == *ptr) || ('\n' == *ptr))
-            break;
-    }
-
-    return chs;
-}
+#define FUNC_CHS        0
+#define FUNC_CAR        1
+#define FUNC_SELECT     FUNC_CAR
 
 
-int main(int argc, char *argv[])
-{
-    uint32_t chs = 0;
-    uint32_t duty = 0;
-    uint32_t period = 3000;
-    uint32_t ctrl = 0;
 
-    pru_start(PATH_PRU0_0);
-    pru_start(PATH_PRU0_1);
-    pru_start(PATH_PRU1_0);
-    pru_start(PATH_PRU1_1);
-    pwm_init();
-
-    chs = select_chs();
-    if (0 == chs)
-        goto STOP;
-
-    printf("Selected:0x%04X\n('q' - exit, '+' - inc, '-' - dec, 'r' - run/stop)\n", chs);
-    pwm_duty(chs, duty);
-    pwm_peroid(chs, period);
-    pwm_ctrl(chs, 0x00);
-
-    motor_init();
-    motor_state(2);
-
-    for (;;) {
-        switch (getchar())
-        {
-        case 'q':
-        case 'Q':
-            goto STOP;
-
-        case 'r':
-        case 'R':
-            ctrl = !ctrl;
-            pwm_ctrl(chs, ctrl);
-            printf("ctrl=%d\n", ctrl);
-        break;
-
-        case '+':
-            if (duty < period) {
-                duty+=100;
-                pwm_duty(chs, duty);
-                printf("duty=%d\n", duty);
-            }
-        break;
-
-        case '-':
-            if (duty > 0) {
-                duty-=100;
-                pwm_duty(chs, duty);
-                printf("duty=%d\n", duty);
-            }
-        break;
-        }
-    }
-
-STOP:
-    printf("Stopped!!!\n");
-    motor_deinit();
-    pwm_ctrl(PWM_ALL, 0x00);
-    pwm_deinit();
-    pru_stop(PATH_PRU0_0);
-    pru_stop(PATH_PRU0_1);
-    pru_stop(PATH_PRU1_0);
-    pru_stop(PATH_PRU1_1);
-
-    return 0;
-}
-#else
-
+#if (FUNC_SELECT == FUNC_CAR)
 #define MOTOR_PERIOD    3000
 #define MOTOR_CHS       (0x01<<0)
 
@@ -259,6 +164,107 @@ STOP:
     pru_stop(PATH_PRU0_1);
     pru_stop(PATH_PRU1_0);
     pru_stop(PATH_PRU1_1);
+}
+
+#else
+
+uint32_t select_chs(void)
+{
+    uint32_t chs = 0;
+    char str[64];
+    char *ptr = NULL;
+    printf("Please select pwm chs[0-%d]:(0,1,2,3...)\n", PWM_MAX_CHS-1);
+    fgets(str, sizeof(str), stdin);
+    if ((str[0] == 'q') || (str[0] == 'Q'))
+        return 0;
+
+    ptr = str;
+    while (*ptr) {
+        int i = atoi(ptr);
+        if (i < 32) {
+            chs |= ((uint32_t)0x01<<i);
+        }
+
+        ptr = strchr(ptr, ',');
+        if (NULL == ptr)
+            break;
+        ptr++;
+
+        if (('\r' == *ptr) || ('\n' == *ptr))
+            break;
+    }
+
+    return chs;
+}
+
+int main(int argc, char *argv[])
+{
+    uint32_t chs = 0;
+    uint32_t duty = 0;
+    uint32_t period = 3000;
+    uint32_t ctrl = 0;
+
+    pru_start(PATH_PRU0_0);
+    pru_start(PATH_PRU0_1);
+    pru_start(PATH_PRU1_0);
+    pru_start(PATH_PRU1_1);
+    pwm_init();
+
+    chs = select_chs();
+    if (0 == chs)
+        goto STOP;
+
+    printf("Selected:0x%04X\n('q' - exit, '+' - inc, '-' - dec, 'r' - run/stop)\n", chs);
+    pwm_duty(chs, duty);
+    pwm_peroid(chs, period);
+    pwm_ctrl(chs, 0x00);
+
+    motor_init();
+    motor_state(2);
+
+    for (;;) {
+        switch (getchar())
+        {
+        case 'q':
+        case 'Q':
+            goto STOP;
+
+        case 'r':
+        case 'R':
+            ctrl = !ctrl;
+            pwm_ctrl(chs, ctrl);
+            printf("ctrl=%d\n", ctrl);
+        break;
+
+        case '+':
+            if (duty < period) {
+                duty+=100;
+                pwm_duty(chs, duty);
+                printf("duty=%d\n", duty);
+            }
+        break;
+
+        case '-':
+            if (duty > 0) {
+                duty-=100;
+                pwm_duty(chs, duty);
+                printf("duty=%d\n", duty);
+            }
+        break;
+        }
+    }
+
+STOP:
+    printf("Stopped!!!\n");
+    motor_deinit();
+    pwm_ctrl(PWM_ALL, 0x00);
+    pwm_deinit();
+    pru_stop(PATH_PRU0_0);
+    pru_stop(PATH_PRU0_1);
+    pru_stop(PATH_PRU1_0);
+    pru_stop(PATH_PRU1_1);
+
+    return 0;
 }
 
 #endif
